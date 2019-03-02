502b23a0ca4ba02e3f7db8b01f86d7bab73eb1cf
syouhei <syouhei@Gizumo002.local>
2019-03-02 16:34:50 +0900

    first commit

---
+++ swift-lesson2/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  swift-lesson2
+//
+//  Created by syouhei on 2019/03/02.
+//  Copyright © 2019 syouhei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("ありがとう")
+        
+    }
+
+
+}
+
